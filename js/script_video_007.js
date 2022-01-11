f03c2a0c15ace3830a421f95b665b44619273c58
Maxie <maxie.simonen@gmail.com>
2022-01-11 14:58:44 +0200

    first commit

---
+++ js/script_video_007.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = prompt('Are you 18 years old?', '18');
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your second name?', '');
+// answers[2] = prompt('What is your age?', '');
+
+console.log(typeof(answers));
